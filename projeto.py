ed4971e219304b206f5ea3df2ce3117a4d36b5e1
Ivan <ivanbarsoa910@gmail.com>
2025-11-09 22:19:29 -0300

    6 commt- troquei de txt por json para ficar mais facil

--- projeto.py
+++ projeto.py
@@ -1,4 +1,5 @@
 import os
+import json
 import time as tm
 def menu():
     while True:
@@ -9,7 +10,7 @@ def menu():
         print("")
         print("1️⃣ -Cadastrar")
         print("2️⃣ -Entrar")
-        print("➜ 🚪 -Sair")
+        print("3️⃣ -Sair")
         print("")
         entrada1 = int(input())
         while entrada1 <= 0 or entrada1 >= 4:
@@ -21,7 +22,7 @@ def menu():
             print("")
             print("1️⃣ -Cadastrar")
             print("2️⃣ -Entrar")
-            print("➜ 🚪 -Sair")
+            print("3️⃣ -Sair")
             print("")
             entrada1 = int(input())
         if entrada1 == 1:
@@ -31,93 +32,109 @@ def menu():
         else:
             os.system('cls')
             break
+def menurAlternativo():
+    while True:
+        os.system('cls')
+        print("===========================MENU===========================")
+        print("")
+        print("Digite qual sua escolha!!")
+        print("")
+        print("1️⃣ -Produtos")
+        print("2️⃣ -Carrinho")
+        print("3️⃣ -Sair")
+        print("")
+        entrada1 = int(input())
+        if entrada1 <= 0 or entrada1 >= 4:
+            while True:
+                os.system('cls')
+                print("Número inválido!!")
+                print("Por favor digite um número válido!!!")
+                print("===========================MENU===========================")
+                print("Digite qual sua escolha!!")
+                print("")
+                print("1️⃣ -Produtos")
+                print("2️⃣ -Carrinho")
+                print("3️⃣ -Sair")
+                print("")
+                entrada1 = int(input())
+                if entrada1 == 1:
+                    pass
+                if entrada1 == 2:
+                    pass
+                else:
+                    os.system('cls')
+                    return
+def menurPrincipal():
+        os.system('cls')
+        print("BEM-VINDO!!")
+        tm.sleep(3)
+        #os.system('cls')
+        print("!MENU!")
+        print("Digite 4 para voltar para o MENU!!")
+        voltarParaMenur = int(input())
+        if voltarParaMenur == 4:
+            menurAlternativo()
 def cadastrar():
     os.system('cls')
     print("========================CADASTRAR========================")
-    listaDeUser = []
-    nomeNovo = input("Nome: ")
-    while True:
-        logins = open("logins.txt", "r")
-        #logins.white("Primeira linha do arquivo.\n Segunda linha do arquivo.\n")
-        lines = logins.readlines()
-        for contas in lines:
-            pass
-        logins.close()
-        if nomeNovo in contas:
+    with open("logins.json", "r") as arquivo:
+        usuarios = json.load(arquivo)
+    nomeNovo = input("Nome: ").strip().lower()
+    for u in usuarios:
+        if nomeNovo == u["nome"]:
             print("\033[1mNOME EXISTENTE\033[0m")
-            nomeNovo = input("Nome: ")
-        else:
-            break
+            return cadastrar()
     print("OBS: digite apenas os numeros do CPF!!")
-    cpf = input("CPF: ")
-    while True:
-        logins = open("logins.txt", "r")
-        #logins.white("Primeira linha do arquivo.\n Segunda linha do arquivo.\n")
-        lines = logins.readlines()
-        for contas in lines:
-            pass
-        logins.close()
-        if cpf in contas:
+    cpf = input("CPF: ").strip().lower()
+    for u in usuarios:
+        if cpf == u["cpf"]:
             print("\033[1mCPF JA REGISTRADO\033[0m")
-            cpf = input("CPF: ")
-        else:
-            break
+            return cadastrar()
     contadorCPF = 0
     for i in cpf:
         contadorCPF += 1
-    idade = int(input("Idade: "))
+    idade = int(input("Idade: ").lower().strip())
     while idade <3 and idade>150:
         print("Idade invalida!!")
         idade = int(input("Idade: "))
         if idade >3 and idade<150:
             break
-    print("Senha deve conter 8 digitos ou caracteres")
-    senhaDeUser = input("Senha: ")
+    print("\033[1mSenha deve conter no minimo 8 digitos ou caracteres\033[0m")
+    senhaNova = input("Senha: ").strip()
     contadorSenha = 0
-    for i in senhaDeUser:
-        contador2 +=1
+    for i in senhaNova:
+        contadorSenha +=1
     if idade > 18 and contadorCPF == 11 and contadorSenha == 8:
-        contador=0
-        os.system('cls')
-        tm.sleep(1)
-        print("BEM-VINDO!!")
-        logins = open("logins.txt", "a")
-        nomeDeUser = str(nome.strip().lower())
-        listaDeUser.append(nomeDeUser)
-        listaDeUser.append(senhaDeUser)
-        listaDeUser.append(idade)
-        listaDeUser.append(cpf)
-        cadastro = str(listaDeUser)
-        logins.writelines(f"\n{cadastro}")
-        logins.close()
-        tm.sleep(5)
-        os.system('cls')
-        print("!MENU!")
-        print("Digite 4 para voltar para o MENU!!")
-        voltarParaMenur = int(input())
-        if voltarParaMenur == 4:
-            menu()
+        novoUsuario = {
+            "nome": nomeNovo.strip().lower(),
+            "senha": senhaNova.strip(),
+            "idade": idade,
+            "cpf": cpf
+        }
+        usuarios.append(novoUsuario)
+        with open("logins.json", "w") as arquivo:
+            json.dump(usuarios, arquivo)
+        menurPrincipal()
     else:
         menu()
 def entrar():
     os.system('cls')
     print("==========================ENTRAR==========================")
-    nome = input("User: ")
-    senha = input("Senha: ")
     while True:
-        logins = open("logins.txt", "r")
-        #logins.white("Primeira linha do arquivo.\n Segunda linha do arquivo.\n")
-        lines = logins.readlines()
-        for contas in lines:
-            pass
-        logins.close()
-        if nome in contas:
-            print("\033[1mUSER EXISTENTE\033[0m")
+        fechar = 0
+        with open("logins.json", "r") as arquivo:
+            usuarios = json.load(arquivo)
+        nome = input("User: ")
+        senha = input("Senha: ")
+        for u in usuarios:
+            if nome == u["nome"] and senha == u["senha"]:
+                print("\033[1mUSER EXISTENTE\033[0m")
+                fechar = 1
+                break
+            else:
+                print("\033[1mNOME OU SENHA INVÁLIDO!!\033[0m")
+                print("==========================ENTRAR==========================")
+        if fechar == 1:
             break
-        else:
-            print("\033[1mUSER NÃO EXISTENTE\033[0m")
-            print("Você sera redirigido para a pagina de login")
-            tm.sleep(10)
-            print("USER NÃO EXISTENTE")
-    seila = input()
+    menurPrincipal()
 menu()
